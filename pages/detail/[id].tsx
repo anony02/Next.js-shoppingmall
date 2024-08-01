0b527db1dd1d221e6403f4d31e3beb606bf5e076
DESKTOP-S9RH0NN\I <jmjml15@naver.com>
2024-08-01 16:55:21 +0900

    🐛 파일 이동
    파일 이동

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import Detail from '../components/Detail';
+import Detail from '../../components/Detail';
 
 const DetailPage: React.FC = () => {
   const router = useRouter();
